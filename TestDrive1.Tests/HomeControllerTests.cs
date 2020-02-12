d662601d380d07d350ef2c561f55c7f315153300
anmesk <an_mesk@live.com>
2020-02-12 09:13:15 -0500

    Initial configuration, added HomeController test

---
+++ TestDrive1.Tests/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace TestDrive1.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Hello_World()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.Equal("Hello World!", result);
+        }
+    }
+}
--- TestDrive1/Startup.cs
+++ TestDrive1/Startup.cs
@@ -7,15 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace TestDrive1
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +34,8 @@ namespace TestDrive1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
